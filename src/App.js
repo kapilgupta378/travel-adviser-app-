19c6b9ee7fb34b83f408acef875f6ab21d46f2c7
kapil <kapil0506gupta@gmail.com>
2021-09-02 12:32:29 +0530

    added  list

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { CssBaseline, Grid } from "@material-ui/core";
 
-// import { getPlacesData, getWeatherData } from './api/travelAdvisorAPI';
+import { getPlacesData, getWeatherData } from "./api/travelAdvisorAPI";
 import Header from "./components/Header/Header";
 // import List from './components/List/List';
 import Map from "./components/Map/Map";
@@ -11,7 +11,7 @@ const App = () => {
   //   const [rating, setRating] = useState('');
 
   const [coords, setCoords] = useState({});
-  //   const [bounds, setBounds] = useState(null);
+  const [bounds, setBounds] = useState(null);
 
   //   const [weatherData, setWeatherData] = useState([]);
   //   const [filteredPlaces, setFilteredPlaces] = useState([]);
@@ -19,7 +19,7 @@ const App = () => {
 
   // const [autocomplete, setAutocomplete] = useState(null);
   //   const [childClicked, setChildClicked] = useState(null);
-  //   const [isLoading, setIsLoading] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
 
   useEffect(() => {
     navigator.geolocation.getCurrentPosition(
@@ -35,22 +35,22 @@ const App = () => {
   //     setFilteredPlaces(filtered);
   //   }, [rating]);
 
-  //   useEffect(() => {
-  //     if (bounds) {
-  //       setIsLoading(true);
+  useEffect(() => {
+    if (bounds) {
+      setIsLoading(true);
 
-  //       getWeatherData(coords.lat, coords.lng)
-  //         .then((data) => setWeatherData(data));
+      getWeatherData(coords.lat, coords.lng).then((data) =>
+        setWeatherData(data)
+      );
 
-  //       getPlacesData(type, bounds.sw, bounds.ne)
-  //         .then((data) => {
-  //           setPlaces(data.filter((place) => place.name && place.num_reviews > 0));
-  //           setFilteredPlaces([]);
-  //           setRating('');
-  //           setIsLoading(false);
-  //         });
-  //     }
-  //   }, [bounds, type]);
+      getPlacesData(type, bounds.sw, bounds.ne).then((data) => {
+        setPlaces(data.filter((place) => place.name && place.num_reviews > 0));
+        setFilteredPlaces([]);
+        setRating("");
+        setIsLoading(false);
+      });
+    }
+  }, [bounds, type]);
 
   // const onLoad = (autoC) => setAutocomplete(autoC);
 
@@ -67,7 +67,7 @@ const App = () => {
       <Header />
       <Grid container spacing={3} style={{ width: "100%" }}>
         <Grid item xs={12} md={4}>
-          {/* <List
+          <List
             isLoading={isLoading}
             childClicked={childClicked}
             places={filteredPlaces.length ? filteredPlaces : places}
@@ -75,7 +75,7 @@ const App = () => {
             setType={setType}
             rating={rating}
             setRating={setRating}
-          /> */}
+          />
         </Grid>
         <Grid
           item
